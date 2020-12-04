9b8010462da4c452a5bce2d934059a49b38cc57e
aldenho52 <69052933+aldenho52@users.noreply.github.com>
2020-12-04 10:08:17 -0700

    Started Stretch

--- piggybank/src/main/java/com/lambdaschool/piggybank/piggybank/controllers/CoinController.java
+++ piggybank/src/main/java/com/lambdaschool/piggybank/piggybank/controllers/CoinController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -56,4 +57,8 @@ public class CoinController
 
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
+
+    //    http://localhost:2019/money/{amount}
+    @PutMapping(value = "/money/{amount}", produces = "application/json")
+
 }
